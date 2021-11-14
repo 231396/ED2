4f4b1ff44d4524ac67f2baf848e36dc4f77d4673
AndreSacilotto <andre.sacilotto.dev@gmail.com>
2021-11-13 23:25:33 -0300

    reset

--- ED/ED.cpp
+++ ED/ED.cpp
@@ -1,19 +1,8 @@
 
 #include <iostream>
-#include "mains.h"
-
 
 int main()
 {
-    //accountMain();
-
-    //circleMain();
-
-    //opMain();
-
-    //rationalMain();
-
-    //timeMain();
 
     auto x = getchar();
     x = getchar();
